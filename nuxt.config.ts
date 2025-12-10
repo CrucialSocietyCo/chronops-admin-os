eabb2c1acbc3e5dbbc8407f5723f1b769cbed3e4
Bobby Perkins Jr. <CrucialSocietyCo@gmail.com>
2025-12-10 13:08:04 -0600

    cors fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,12 +21,6 @@ export default defineNuxtConfig({
   routeRules: {
     '/api/**': {
       cors: true,
-      headers: {
-        'Access-Control-Allow-Origin': 'http://localhost:5173',
-        'Access-Control-Allow-Credentials': 'true',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-        'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info, apikey'
-      }
     },
   },
 
---
+++ server/middleware/cors.ts
@@ -0,0 +1,22 @@
+export default defineEventHandler((event) => {
+    const allowedOrigins = [
+        'http://localhost:5173',
+        'https://chronops-retrochat-vue.vercel.app'
+    ]
+    const origin = getRequestHeader(event, 'origin')
+
+    if (origin && allowedOrigins.includes(origin)) {
+        setResponseHeaders(event, {
+            'Access-Control-Allow-Origin': origin,
+            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+            'Access-Control-Allow-Headers': 'Content-Type, Authorization, x-client-info, apikey',
+            'Access-Control-Allow-Credentials': 'true'
+        })
+    }
+
+    if (getMethod(event) === 'OPTIONS') {
+        event.node.res.statusCode = 204
+        event.node.res.statusMessage = 'No Content'
+        return 'OK'
+    }
+})
