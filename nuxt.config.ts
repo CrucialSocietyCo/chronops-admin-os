3a2416346beb4b1c73307b8c393a214140f06ceb
Bobby Perkins Jr. <CrucialSocietyCo@gmail.com>
2025-12-10 21:56:39 -0600

    feat: add open graph meta tags and image

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,12 @@ export default defineNuxtConfig({
       title: 'Society on South Main - Admin',
       link: [
         { rel: 'icon', type: 'image/jpeg', href: '/favicon_admin.jpg' }
+      ],
+      meta: [
+        { property: 'og:title', content: 'Society on South Main - Admin' },
+        { property: 'og:description', content: 'Administrative Dashboard for Society on South Main.' },
+        { property: 'og:image', content: 'https://api.southmain.app/og-image.png' },
+        { property: 'og:type', content: 'website' }
       ]
     }
   },
